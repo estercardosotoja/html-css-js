53361499e155f88f48e9198f93893eab53413bef
Ester Toja <ester.toja@hotmail.com>
2025-04-24 17:39:21 -0300

    parte1

--- form_funcionario/js/firebaseConfig.js
+++ form_funcionario/js/firebaseConfig.js
@@ -4,13 +4,13 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/11.6.0/firebase
 
 // Configuração correta do Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyDl-9nDWzYH9YQ71Kx6eNRBdPgzNRxgjfU",
-  authDomain: "teste2-ba2d6.firebaseapp.com",
-  databaseURL: "https://teste2-ba2d6-default-rtdb.firebaseio.com",
-  projectId: "teste2-ba2d6",
-  storageBucket: "teste2-ba2d6.appspot.com", // Corrigido aqui
-  messagingSenderId: "504728021083",
-  appId: "1:504728021083:web:faf1e856609d7a9c0bb77e"
+  apiKey: " ",
+  authDomain: " ",
+  databaseURL: " ",
+  projectId: " ",
+  storageBucket: " ", 
+  messagingSenderId: " ",
+  appId: " "
 };
 
 // Inicializa o app
